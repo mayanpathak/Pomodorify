76bf7ee084b0d9a5b0897a5dec0f8b185ed1967a
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-06 05:43:38 +0530

    new code cleanup

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -1,17 +1,4 @@
-// import { MailtrapClient } from "mailtrap";
-// import dotenv from "dotenv";
 
-// dotenv.config();
-
-// export const mailtrapClient = new MailtrapClient({
-// 	endpoint: process.env.MAILTRAP_ENDPOINT,
-// 	token: process.env.MAILTRAP_TOKEN,
-// });
-
-// export const sender = {
-// 	email: "hello@demomailtrap.com",
-// 	name: "Mayan",
-//   };
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
